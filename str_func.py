d600cca4df4a07e85669a18f9d89868857b25292
Nail Razakov Win10 <nailrazakov1982@gmail.com>
2023-10-15 23:00:01 +0300

    Создали функцию, которая первую букву слова делает заглавной

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def upper_make(word: str) -> str:
     return word.upper()
+
+def upper_make_title(word: str) -> str:
+    """Функция возвращает строку со словами начинающихся с заглавной"""
+    return word.title()
